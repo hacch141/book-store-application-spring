e27ad06d14773bc3235259aa6568aea6341aee9e
hacch141 <harsh.j.chaudhari141@gmail.com>
2026-08-24 00:52:09 +0530

    feat(catalog): scaffold the catalog service

---
+++ catalog-service/src/test/java/com/harsh/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.harsh.bookstore.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
